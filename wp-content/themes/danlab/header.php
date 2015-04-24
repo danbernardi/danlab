ad19e4459c32564a0a150d74ea06e6d55a16d1fa
Daniel Bernardi <dan@zendenwebdesign.com>
2015-04-24 16:57:18 -0700

    adds google tracking code

--- wp-content/themes/danlab/header.php
+++ wp-content/themes/danlab/header.php
@@ -9,6 +9,7 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+	<?php include_once("analyticstracking.php") ?>
 	<section id="pagewrap">
 	<div class="loader"><div><i class="fa fa-spinner fa-spin"></i></div></div>
 	<div class="mob-menu">
